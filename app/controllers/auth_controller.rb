5657b2260c67c491e7288c97c82acf0e30953a1e
Maria Cassino <maria.cassino@gmail.com>
2016-06-29 15:59:07 -0400

    generated home controller and root

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,2 +1,16 @@
 class AuthController < ApplicationController
+
+  def twilio
+      data = request.env["omniauth.auth"]
+
+      user = User.where(twilio_id: data.uid).first_or_create! do |u|
+        u.email       = data.info.email
+        u.password    = SecureRandom.hex 64
+        u.twilio_data = data.to_h.to_json
+      end
+
+      sign_in user
+      redirect_to "/", notice: "Signed in with Twilio"
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  root 'home#number'
+
   # post "/messages/:id" => "posts#create"
 
   # The priority is based upon order of creation: first created -> highest priority.
