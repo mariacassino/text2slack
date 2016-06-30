691b16f9b57715e69fb16b67e7fe01f41f130db2
Maria Cassino <maria.cassino@gmail.com>
2016-06-30 13:32:13 -0400

    working Slack sign-in button

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,11 +1,9 @@
 class CallbacksController < Devise::OmniauthCallbacksController
-    # def slack
-    #     @user = User.from_omniauth(request.env["omniauth.auth"])
-    #     sign_in_and_redirect @user
-    # end
 
     def slack
       request.env["omniauth.auth"]
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+
         # data = request.env["omniauth.auth"]
         #
         # user = User.where(slack_id: data.uid).first_or_create! do |u|
@@ -14,8 +12,8 @@ class CallbacksController < Devise::OmniauthCallbacksController
         #   # u.twilio_data = data.to_h.to_json
         # end
         #
-        # sign_in user
-        # redirect_to "/", notice: "Signed in with Slack"
+        sign_in @user
+        redirect_to "/", notice: "Signed in with Slack"
       end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,13 @@ class User < ActiveRecord::Base
 
 
          def self.from_omniauth(auth)
-             where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-               user.provider = auth.provider
+             where(uid: auth.uid).first_or_create do |user|
                user.uid = auth.uid
                user.email = auth.info.email
                user.password = Devise.friendly_token[0,20]
+               user.slack_token = auth.credentials.token
              end
          end
-      
+
 
 end
