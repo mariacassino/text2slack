e8a1f3bcc20b6b2f5646a6e673e8aa59feb0744b
Maria Cassino <maria.cassino@gmail.com>
2016-07-04 15:25:09 -0400

    added Text2Slack logo to form and signout pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource_or_scope)
     home_form_path
   end
-  
+
   def after_sign_out_path_for(resource_or_scope)
     home_goodbye_path
   end
