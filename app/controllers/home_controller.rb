db2d516c22de20442109917477a680c40a0f4355
Maria Cassino <maria.cassino@gmail.com>
2016-07-03 21:18:04 -0400

    created GET request to channels.list API; user can see their channels

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
 
+
+
 def form
   @user = current_user
+  if @user
+    all_channels = channels_query["channels"]
+    @channel_list = all_channels.map{|x| x["name"]}
+  end
 end
 
 
@@ -13,6 +19,19 @@ def update
   end
 end
 
+
+def channels_query
+  @user = current_user
+  if @user
+  @token = @user.slack_token
+  request = HTTParty.get("https://slack.com/api/channels.list",
+            headers: { "Authorization" => @token,
+                      "User-Agent" => "Maria"
+                      },
+            query:   {token: @token,
+                      })
+                    end
+  end
 
 
 
