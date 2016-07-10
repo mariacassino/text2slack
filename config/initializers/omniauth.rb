cd61755e97fd100c318c9fd83109e70be4738dcf
Maria Cassino <maria.cassino@gmail.com>
2016-07-10 11:14:02 -0400

    configured ENV stuff

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#   provider :developer unless Rails.env.production?
-#   provider :slack, "APP_ID", "APP_SECRET", scope: "client"
-# end
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :developer unless Rails.env.production?
+  provider :slack, ENV["SLACK_APP_ID"], ENV['SLACK_APP_SECRET"], scope: "client"
+end
